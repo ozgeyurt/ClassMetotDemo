903ab1f4d80bfd4e378dc1e908e3b3c07cec33b6
ozgeyurt <ozge.yurt@windowslive.com>
2021-01-24 23:32:08 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+        public void AddMusteri (Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id+" "+ musteri.FirstName+" "+musteri.LastName+" "+"isimli müşteri"+
+                " "+musteri.AccountNumber+ " "+"hesap numarasıyla eklendi");
+            Console.WriteLine("********");
+        }
+        public void UpdateMusteri(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.FirstName + " " + musteri.LastName + " " + "isimli müşteri" + 
+                " " + musteri.AccountNumber + " " + "hesap numarasıyla güncellendi");
+            Console.WriteLine("********");
+
+        }
+        public void DeleteMusteri(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.FirstName + " " + musteri.LastName + " " + "isimli müşteri" +
+               " " + musteri.AccountNumber + " " + "hesap numarasıyla silindi");
+            Console.WriteLine("********");
+
+        }
+        public void ListMusteri(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Id + " " + musteri.FirstName + " " + musteri.LastName + " " + "isimli müşteri" +
+              " " + musteri.AccountNumber + " " + "hesap numarasıyla listeye eklendi");
+
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri() { Id = 1, FirstName = "Ayşe", LastName = "İnan", AccountNumber = 54587 };
+            Musteri musteri2 = new Musteri() { Id = 2, FirstName = "Anıl", LastName = "Altunyurt", AccountNumber = 85468 };
+            Musteri musteri3 = new Musteri() { Id = 3, FirstName = "Özlem", LastName = "Şahin", AccountNumber = 63987 };
+            Musteri musteri4 = new Musteri() { Id = 4, FirstName = "Hakan", LastName = "Kanak", AccountNumber = 12756 };
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.AddMusteri(musteri1);
+            musteriManager.UpdateMusteri(musteri4);
+            musteriManager.DeleteMusteri(musteri2);
+
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3, musteri4 };
+            foreach (var musteri in musteriler)
+            {
+                musteriManager.ListMusteri(musteri);
+            }
+
+        }
+        
+    }
+}
